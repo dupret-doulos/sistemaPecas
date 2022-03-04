0a667a70566697f371dc854b472855a8985fce4d
Dupret <jvdupret02122003@gmail.com>
2022-03-04 11:29:13 -0300

    sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,26 @@
+let pesoPeca = 50 
+
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100g, logo, podemos cadastrar a peça!!!")
+} else {
+console.log("peso insuficiente, não é possível cadastrar.")
+}
+
+let numeroPecas= 10
+
+if(numeroPecas < 10) {
+    console.log(" ainda há espaço na caixa, podemos cadastrar a peça") 
+} else {
+    console.log("Não há espaço disponivel na caixa")
+} 
+
+let nomePeca = "di"
+
+console.log("O comprimento do nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3) {
+    console.log("Nome de peça muito curto, não é possível cadastrar")
+} else {
+    console.log("nome de peça adequado, podemos cadastrar")
+}
